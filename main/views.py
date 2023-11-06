f13bfc0009560d30c160279bf81ad8a774cd781d
blue <12345@gmail.com>
2023-11-06 21:57:32 +0800

    發票兌獎

--- main/views.py
+++ main/views.py
@@ -10,10 +10,55 @@ from django.views.decorators.csrf import csrf_exempt
 
 from linebot import LineBotApi, WebhookHandler, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextSendMessage
+from linebot.models import MessageEvent, TextSendMessage, ImageSendMessage
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parse = WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+
+@csrf_exempt
+def callback(request):
+    if request.method == "POST":
+        signature = request.META["HTTP_X_LINE_SIGNATURE"]
+        body = request.body.decode("utf-8")
+        try:
+            events = parse.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+        for event in events:
+            if isinstance(event, MessageEvent):
+                message = event.message.text
+                message_object = None
+
+                if message == "你好":
+                    message_object = TextSendMessage(text="你也好!")
+                elif message == "樂透":
+                    message_object = TextSendMessage(text=get_lottory_num())
+                elif "捷運" in message:
+                    if "台北" in message:
+                        url = "https://kuopoting.github.io/k1/assets/20200119_zh.png"
+                    elif "台中" in message:
+                        url = "https://assets.piliapp.com/s3pxy/mrt_taiwan/taichung/20201112_zh.png?v=2"
+                    elif "高雄" in message:
+                        url = "https://assets.piliapp.com/s3pxy/mrt_taiwan/kaohsiung/202210_zh.png"
+                    message_object = ImageSendMessage(
+                        original_content_url=url, preview_image_url=url
+                    )
+                else:
+                    reply_message = "我不懂你的意思"
+                    message_object = TextSendMessage(text=reply_message)
+
+                # if event.message.text=='hello':
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    # TextSendMessage(text='hello world')
+                    message_object,
+                )
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
 
 
 def index(request):
@@ -32,6 +77,13 @@ def get_lottory2(request):
     x = random.randint(1, 50)
     lottorynum_str = " ".join(map(str, lottorynum))
     return render(request, "lottory.html", {"x": x, "lottorynum": lottorynum_str})
+
+
+def get_lottory_num():
+    lottorynum = sorted(random.sample(range(1, 50), 6))
+    x = random.randint(1, 50)
+    lottorynum_str = " ".join(map(str, lottorynum))
+    return lottorynum_str
 
 
 def get_lottory(request):
